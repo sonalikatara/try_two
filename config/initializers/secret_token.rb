106b9cb4e6a449639834f7e550139f7dad3fee4e
sonali <sonalikatara@gmail.com>
2013-10-31 13:02:12 -0700

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TryTwo::Application.config.secret_key_base = '1b6955fa4aa8f5e24f1b1fd8b78e4aa59bd16f5edb69649cda7003bde9eb0d1db3a6f625fcaac804cf33b30d0e20234a0c300e10676f99a5412c2242a5782c56'
